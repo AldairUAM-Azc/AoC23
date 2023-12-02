a79a4aaa448e36e8de43b73898adbed4d1f63927
AldairUAM-Azc <108377001+AldairUAM-Azc@users.noreply.github.com>
2023-12-02 17:05:14 +0000

    Day2: setup

--- day1/Day1Aldair.java
+++ day1/Day1Aldair.java
@@ -2,46 +2,69 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Day1Aldair {
-    public static int findFirstDigit(String line){
-        for(int i = 0; i < line.length(); i++){
+    public static int findIndexFirstDigit(String line) {
+        for (int i = 0; i < line.length(); i++) {
             char currSymbol = line.charAt(i);
-            if(currSymbol > 47 && currSymbol < 58){
-                return currSymbol % 48;
+            if (currSymbol > 47 && currSymbol < 58) {
+                return i;
             }
         }
         return -1;
     }
 
-    public static int findLastDigit(String line){
-        for (int i =line.length()-1; i >= 0;i--){
+    public static int findIndexLastDigit(String line) {
+        for (int i = line.length() - 1; i >= 0; i--) {
             char currSymbol = line.charAt(i);
-            if (currSymbol >47 && currSymbol < 58) {
-                return currSymbol % 48;
+            if (currSymbol > 47 && currSymbol < 58) {
+                return i;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) {
+        String[] numberNames = new String[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        HashMap<Integer, Integer> indexDigitMap = new HashMap<>();
+
         FileReader fr;
         BufferedReader br;
         try {
             fr = new FileReader("/workspaces/AoC23/day1/aldair_input_day1.txt");
             br = new BufferedReader(fr);
-            int sum = 0;
+            int result = 0;
             while (br.ready()) {
                 String line = br.readLine();
-                int firstDigit = findFirstDigit(line);
-                int lastDigit = findLastDigit(line);
-                int num = firstDigit*10 + lastDigit;
-                sum += num;
+                int idxFirstDigit = findIndexFirstDigit(line);
+                int idxLastDigit = findIndexLastDigit(line);
+                indexDigitMap.put(idxFirstDigit, Character.getNumericValue(line.charAt(idxFirstDigit)));
+                indexDigitMap.put(idxLastDigit, Character.getNumericValue(line.charAt(idxLastDigit)));
+                // Find the number in letter inside line
+                for (int i = 0; i < numberNames.length; i++) {
+                    int position = line.indexOf(numberNames[i]);
+                    if (position != -1) {
+                        indexDigitMap.put(position, i+1);
+                    }
+                }
+                
+                Set<Integer> keySet = indexDigitMap.keySet();
+                // System.out.println("START");
+                // System.out.println(indexDigitMap);
+                // System.out.println(Collections.min(keySet) + indexDigitMap.get(Collections.max(keySet)));
+                // System.out.println(indexDigitMap.get(Collections.min(keySet))*10 + indexDigitMap.get(Collections.max(keySet)));
+                result += indexDigitMap.get(Collections.min(keySet))*10 + indexDigitMap.get(Collections.max(keySet));
+                indexDigitMap.clear();
             }
-            System.out.println(sum);
+            System.out.println(result);
             br.close();
             fr.close();
-        } catch (FileNotFoundException e) {
+        } catch (
+
+        FileNotFoundException e) {
             System.out.println("File not Found");
         } catch (IOException e) {
             System.out.println("IO Exception");
---
+++ day2/Aldair.java
@@ -0,0 +1,5 @@
+public class Aldair {
+    public static void main(String[] args) {
+        
+    }
+}
